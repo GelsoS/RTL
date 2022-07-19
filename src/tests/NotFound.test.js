a4e9e691a9754f42c5abaff9e548bf044634d65f
gelso <gelso02@live.com>
2022-07-19 15:45:03 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../utils/renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/test');
+      const h2 = screen.getByRole('heading', {
+        name: /Page requested not found/i,
+        level: 2,
+      });
+      expect(h2).toBeInTheDocument();
+    });
+  it('Teste se a página mostra a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/test');
+
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const alt = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByAltText(alt);
+    // expect(img).toHaveAttribute('alt', 'Pokédex');
+    expect(img).toHaveAttribute('src', src);
+  });
+});
